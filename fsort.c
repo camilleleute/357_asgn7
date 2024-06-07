933f6b7dc1a89992eb0324c5a15454ce560dc2ab
Camille Thuy Leute <cleute@unix2.csc.calpoly.edu>
2024-06-07 15:58:23 -0700

    humma

--- fsort.c
+++ fsort.c
@@ -1,7 +1,11 @@
 /* Defines functions for sorting arrays in parallel.
  * CSC 357, Assignment 7
  * Given code, Winter '24 */
-
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/wait.h>
 #include "fsort.h"
 
 /* fsort: Sorts an array using a parallelized merge sort.
@@ -13,5 +17,83 @@ int fsort(
  void *base, size_t n, size_t width, size_t min,
  int (*cmp)(const void *, const void *))
 {
-    return 0;
+	return mergeSort(base, n, width, min, cmp);
 }
+
+void merge(void* base, size_t mid, size_t n, size_t width,
+  	    int (*cmp)(const void *, const void *)) {
+	char * arr = (char *)base;
+	void * merged = malloc(n * width);
+
+	//make 2 sub arrays
+
+
+	size_t L = 0;
+	size_t R = mid;
+	size_t J = 0;
+
+	while ((L < mid) && (R < n)) {
+		if ((cmp(arr + (L * width), (arr + (R *width)))) <= 0) {
+			memcpy((char *)merged + J * width, arr + L * width, width);
+			L++;
+		} else {
+			memcpy((char*)merged + J*width, arr + R*width, width);
+			R++;
+		}
+		J++;
+	}
+	
+	while (L < mid) {
+		memcpy((char *)merged + J * width, arr + L * width, width);
+                L++;
+		J++;
+	}
+
+	while (R < n) {
+		memcpy((char*)merged + J*width, arr + R*width, width);
+                R++;
+		J++;
+	}
+
+	memcpy(arr, merged, n*width);
+	free(merged);
+} 
+
+int mergeSort(void *base, size_t n, size_t width, size_t min,
+ int (*cmp)(const void *, const void *)) {
+	if (n < 2) return 0;
+	size_t mid = 0;
+	if (n%2 == 1) {
+		mid = ((n-1)/2) - 1;
+	} else {
+		mid = (n/2) - 1;
+	}
+		
+	if (n > min) {
+		int fds[2];
+
+		pid_t pid = fork();
+		if (pid == 0) { //child
+			close(fds[0]);
+			mergeSort((char *)base + mid * width, n - mid, width,min, cmp);
+        		write(fds[1], (char *)base + mid * width, (n - mid) * width);
+        		close(fds[1]);
+        		exit(0);
+		} else { //parent
+			close(fds[1]); 
+	       		mergeSort(base, mid, width,min, cmp);
+        		wait(NULL);
+        		read(fds[0], (char *)base + mid * width, (n - mid) * width);
+        		close(fds[0]);
+        		merge(base, mid, n, width, cmp);
+		}	
+
+	} else {
+		qsort(base, n, width, cmp);
+		return 0;
+	}
+	return 0;
+}
+
+
+
--- fsort.h
+++ fsort.h
@@ -4,5 +4,6 @@
  * NOTE: Do not alter this file. */
 
 #include <stddef.h>
-
+void merge(void*, size_t, size_t, size_t, int (*cmp)(const void *, const void *));
+int mergeSort(void *, size_t, size_t, size_t, int (*)(const void *, const void *));
 int fsort(void *, size_t, size_t, size_t, int (*)(const void *, const void *));
